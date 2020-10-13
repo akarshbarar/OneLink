e246f85befd347ce7126fd08d001a7f144baedf4
Akarsh Barar <barar.akarsh8@gmail.com>
2020-10-14 02:11:20 +0530

    some css

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,10 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'mylinks',
+    title: 'OneLink',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -26,7 +25,6 @@ export default {
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
--- pages/index.vue
+++ pages/index.vue
@@ -1,78 +1,55 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        mylinks
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+ <div class="index">
+   <div class="index__left">
+     <div class="index__leftData">
+        <p class="index__leftDataHeading">OneLink for All Links</p>
+        <p class="index__leftDataParagraph">Your links at one place</p>
+     </div>
+   </div>
+   <div class="index__right">
+     <div>
+       <p>"Its really awesome!"</p>
+     </div>
+   </div>
+ </div>
 </template>
 
 <script>
-export default {}
+export default {
+
+    // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'OneLink',
+    meta: [
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'description', content: 'OneLink is a site for all your personal links at one place.' },
+      { name: 'keyword', content: 'OneLink socail media facebook instagram snapchat whatsapp website portfolio' },
+    ],
+  },
+}
 </script>
 
 <style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
+.index{
   display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  flex-direction: row;
+}
+.index__left{
+  height: 100vh;
+  width: 50vw;
+  background-color: #1B3D58;
+}
+.index__leftData{
+  padding: 20%;
+}
+.index__leftDataHeading{
+  color: #338ED9;
+  font-size: 50px;
+  font-weight: 800;
+}
+.index__leftDataParagraph{
+  color: white;
+  font-size: 35px;
 }
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
